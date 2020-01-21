9a3bd042a21759feb72755cc1abb8eb030315abc
smhickson <shermainehickson@gmail.com>
2020-01-21 15:13:10 -0600

    Initial Upload to the Dice Game

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+//var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1 through dice 6
+
+var randomImageSource = "images/dice" + randomNumber1 + ".png"; //images/dice1.png through images/dice6.png
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+//image1.setAttribute("src", randomImageSource);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = " Player 1 Wins!";
+}
+
+//If player 2 wins
+else if (randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = " Player 2 Wins!";
+}
+
+//If its a tie
+else {
+  document.querySelector("h1").innerHTML = "Oh Crap..Draw!";
+}
